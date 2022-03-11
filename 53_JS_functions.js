2939e4deb4a539483bfbc05836ad441404a1c2b8
ayush <ayush.26tiwari@gmail.com>
2022-03-11 17:45:56 +0530

    Web Development

---
+++ 53_JS_functions.js
@@ -0,0 +1,24 @@
+console.log("This is video 53 tutorial");
+
+function greet(name, greetText){
+    console.log(greetText + " " + name + ", you are a good person.");
+}
+let greetText = "Good Morning";
+let name1 = "ayush";
+let name2 = "vratant";
+let name3 = "nakul";
+let name4 = "himanshu";
+
+greet(name1, greetText);
+greet(name2, greetText);
+greet(name3, greetText);
+greet(name4, greetText);
+
+//___________________________________________________________________________________________________________________________
+
+function sum(a, b, c){
+    let d = a+b+c;
+    return d;
+}
+let returned_value = sum(65,34,12);
+console.log(returned_value)
